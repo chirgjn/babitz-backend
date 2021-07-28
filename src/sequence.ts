67f972e0da6c5dd8a9ba149b30b0d6375b28f186
jagdeepak <jagdeepak009@gmail.com>
2021-07-28 19:49:10 +0530

    style(sequence.ts): eslint check and fix

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,23 +1,23 @@
 import {DefaultSequence, RequestContext, RestHttpErrors} from '@loopback/rest';
 import * as admin from 'firebase-admin';
 
-
 export class MySequence extends DefaultSequence {
+  /* eslint-disable */
   async authenticateToken(request: Request): Promise<any> {
     // console.log("context in middleware", request);
 
     if ((request.headers as any).authorization) {
-      let authToken = (request.headers as any).authorization;
+      const authToken = (request.headers as any).authorization;
       if (authToken) {
         return admin
           .auth()
           .verifyIdToken(authToken)
           .then((token: any) => {
-            console.log("varified ", token);
+            console.log('varified ', token);
             return;
           })
-          .catch(async (error: {message: any;}) => {
-            console.log("error", error);
+          .catch(async (error: {message: any}) => {
+            console.log('error', error);
             return error.message;
           });
       } else {
@@ -28,18 +28,20 @@ export class MySequence extends DefaultSequence {
     }
   }
   async handle(context: RequestContext) {
-    let request: any = context.request;
-    let response: any = context.response;
-    let err = await this.authenticateToken(request);
+    const request: any = context.request;
+    const response: any = context.response;
+    const err = await this.authenticateToken(request);
     if (err != undefined) {
       console.log(err);
       response.status('400');
-
-      return this.reject(context, RestHttpErrors.invalidData(err, "Authorization Header"));
+      return this.reject(
+        context,
+        RestHttpErrors.invalidData(err, 'Authorization Header'),
+      );
     } else {
-      let result = await super.handle(context);
+      const result = await super.handle(context);
       return result;
     }
-
-  };
+  }
+  /* eslint-enable */
 }
