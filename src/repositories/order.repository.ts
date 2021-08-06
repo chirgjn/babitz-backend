6b41db51e6014e24efddefea129de2a3c0bd989a
jagdeepak <jagdeepak009@gmail.com>
2021-08-06 22:51:29 +0530

    feat(added models and repositories.): added models required for babitz. And the crud features

---
+++ src/repositories/order.repository.ts
@@ -0,0 +1,14 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {PgDataSource} from '../datasources';
+import {Order, OrderRelations} from '../models';
+
+export class OrderRepository extends DefaultCrudRepository<
+  Order,
+  typeof Order.prototype.id,
+  OrderRelations
+> {
+  constructor(@inject('datasources.pg') dataSource: PgDataSource) {
+    super(Order, dataSource);
+  }
+}
